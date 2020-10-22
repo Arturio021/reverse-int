470b18bb0afb710de475e78fbddcbd3d394c657f
Artur <korbut.arturio@gmail.com>
2020-10-22 18:56:54 +0300

    init: ready project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    var str = String(n);
+    var arr = str.split("");
+    var arrResult = [];
+    for (var i = arr.length - 1; i >= 0; --i) {
+        arrResult += arr[i];
+    }
+    console.log(arrResult);
+
+    return arrResult;
 }
